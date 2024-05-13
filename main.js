c5050849c0d65bcd19d39851ef1024efce4e2cdb
ANIME001ff <165377015+ANIME001ff@users.noreply.github.com>
2024-05-13 15:55:06 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+// //String
+// let ism = "ali"
+// let familya = "axmidov"
+// console.log(familya);
+// let sahrif = "asliddinovich"
+// let fullname = `Mening ismim ${ism}`;
+// console.log(fullname);
+
+// let a = 12;
+// let b = 14;
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a == b);
+// console.log(a != b);
+
+// let a = +prompt("son kirit");
+// let natija = a > 0 ? "Noldan katta" : "Noldan kichkina";
+// alert(natija);
+// let password = prompt("parolni kirit");
+// alert(password == "12345"? "Welcome" : "Password is not corect")
+
+// let A = +prompt("Son kirit");
+// let natija =A>0? "Musbat" : "Manfiy"
+// alert(natija);
+
+// let A = +prompt("Son kirit");
+// let natija =A == 2 ?     "juft" : "toq";
+// alert(natija);
+
